7285b0409e12cff6765ef5c23b4fbf418cdd3d3f
Emile <emile.vianneyliaud@gmail.com>
2023-03-29 14:49:10 +0200

    update interface and import libraries

---
+++ src/components/TheInterface.vue
@@ -0,0 +1,33 @@
+<script setup>
+
+</script>
+
+
+<template>
+    <div class="main-page">
+      <router-link to="/" class="back-link">Back</router-link>
+      <div class="bottom-text">Small text on the bottom left corner</div>
+      <router-view />
+    </div>
+  </template>
+  
+  <style>
+    .main-page {
+      /* Add styles here */
+    }
+    
+    .back-link {
+        position: absolute;
+        top: 0;
+        left: 0;
+        margin: 20px;
+    }
+    
+    .bottom-text {
+        position: absolute;
+        bottom: 0;
+        left: 0;
+        margin: 20px;
+    }
+  </style>
+  
