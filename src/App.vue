b6865e9f0dd0d9c688d8a8f6c32bc7efc2fd85a8
David Wismer <david.wismer@heig-vd.ch>
2023-03-29 15:52:38 +0200

    Component ressources

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,12 @@
 <script setup>
 import Card from './components/card.vue';
+import ressource from './components/ressource.vue';
+
 </script>
 
 <template>
   <Card></Card>
+  <ressource name="Justice" level=40 url="src/assets/icons/balance.svg"></ressource>
 </template>
 
 <style scoped>
---
+++ src/components/ressource.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { ref } from 'vue';
+
+const props = defineProps({
+    name: String,
+    level: Number,
+    url: String
+})
+
+let percentage = ref(parseInt(props.level) + 2)
+</script>
+
+<template>
+    <h1>{{ name }}</h1>
+    <img :src=url>
+</template>
+
+<style scoped>
+
+</style>
