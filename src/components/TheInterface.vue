4e5444e7a8f40a48a6cd24f621cd61b639e2b13e
Emile <emile.vianneyliaud@gmail.com>
2023-03-30 10:26:13 +0200

    Move everything to the interface

--- src/components/TheInterface.vue
+++ src/components/TheInterface.vue
@@ -6,7 +6,22 @@ import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 /* import specific icons */
 import { faArrowLeft, faUserSecret } from '@fortawesome/free-solid-svg-icons'
 /* add icons to the library */
+import Card from './card.vue';
+import ressource from './ressource.vue';
 library.add(faArrowLeft)
+
+
+//Les noms et les niveaux pour chaque ressources (Les niveaux sont en pourcents)
+const RESSOURCES_NAMES = [
+  { name: 'balance', level: 40 },
+  { name: 'bolt', level: 20 },
+  { name: 'brain', level: 80 },
+  { name: 'bulb', level: 75 },
+  { name: 'engagement', level: 33 },
+  { name: 'handshake', level: 29 },
+  { name: 'network', level: 100 },
+  { name: 'sober', level: 0 }
+]
 
 </script>
 
@@ -15,13 +30,25 @@ library.add(faArrowLeft)
     <div class="main-page">
       <p to="/" class="back-link"><font-awesome-icon icon="fa-solid fa-arrow-left" />  Retour au campement</p>
       <div class="bottom-text">La forêt</div>
-      
+      <div id="ressources">
+        <ressource v-for="name of RESSOURCES_NAMES" :name="name.name" :level="name.level" size="60px"></ressource>
+      </div>
+
+      <div id="cards">
+        <Card name="Card 1" description="Lorem ipsum dolor sit amet, consectetur adipiscing elit."></Card>
+        <Card name="Card 2" description="Lorem ipsum dolor sit amet, consectetur adipiscing elit."></Card>
+        <Card name="Card 3" description="Lorem ipsum dolor sit amet, consectetur adipiscing elit."></Card>
+      </div>
     </div>
   </template>
   
   <style>
+  body {
+    background-color: #b8b7b7;
+  }
     .main-page {
-      /* Add styles here */
+      display: flex;
+      justify-content: center;
     }
     
     .back-link {
@@ -29,6 +56,7 @@ library.add(faArrowLeft)
         top: 0;
         left: 0;
         margin: 20px;
+        cursor: pointer;
     }
     
     .bottom-text {
@@ -36,6 +64,25 @@ library.add(faArrowLeft)
         bottom: 0;
         left: 0;
         margin: 20px;
+    }
+    #cards {
+      display: flex;
+      flex-direction: row;
+      flex-wrap: nowrap;
+      justify-content: normal;
+      align-items: normal;
+      align-content: normal;
+      gap: 32px;
+    }
+
+    #ressources {
+      display: flex;
+      flex-direction: row;
+      justify-content: center;
+      gap: 15px;
+      position: absolute;
+      top: 0;
+      margin: 0 auto;
     }
   </style>
   
