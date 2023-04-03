d63a5eb74a91bcc70464cb1a83c6158d0fc12923
Emile <emile.vianneyliaud@gmail.com>
2023-04-03 09:05:40 +0200

    update mistake

--- src/interfaces/TheCardInterface.vue
+++ src/interfaces/TheCardInterface.vue
@@ -7,8 +7,8 @@ import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 /* import specific icons */
 import { faArrowLeft, faUserSecret } from '@fortawesome/free-solid-svg-icons'
 /* add icons to the library */
-import Card from '../components./components/card.vue';
-import ressource from '../components./components/ressource.vue';
+import Card from '../components/card.vue';
+import ressource from '../components/ressource.vue';
 library.add(faArrowLeft)
 
 
