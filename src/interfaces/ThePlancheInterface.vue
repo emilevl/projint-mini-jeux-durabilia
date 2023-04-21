209e6f87170390aeeba2f6ca752e83506d3d937b
Paul Mairot <paul.mairot@heig-vd.ch>
2023-04-21 17:57:06 +0200

    removing debug mode

--- src/interfaces/ThePlancheInterface.vue
+++ src/interfaces/ThePlancheInterface.vue
@@ -15,7 +15,7 @@ let config = {
         default: "arcade",
         arcade: {
             gravity: { y: 2000 },
-            debug: true,
+            debug: false,
         },
     },
     input: {
